0bb4beb6838fb411d27b06d525df869e72715103
Zoltan <zdebre@gmail.com>
2025-02-21 01:17:07 -0500

    Add Question and FromStr

--- qna-warp/src/main.rs
+++ qna-warp/src/main.rs
@@ -1,3 +1,46 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First question".to_string(),
+        "The content of the first question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    println!("Question: {:?}", question);
 }
