8398afc3e9bb0eae421a26cab0ea94419a192f24
Zoltan <zdebre@gmail.com>
2025-03-15 17:29:54 -0400

    Reading mock data from json

--- qna-warp/src/main.rs
+++ qna-warp/src/main.rs
@@ -1,7 +1,13 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
+use warp::cors::CorsForbidden;
+use warp::http::{Method, Request};
+use warp::reject::Reject;
+use warp::{http::StatusCode, Filter, Rejection, Reply};
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, Hash, PartialEq)]
 struct QuestionId(String);
 
 impl FromStr for QuestionId {
@@ -15,7 +21,7 @@ impl FromStr for QuestionId {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -34,13 +40,71 @@ impl Question {
     }
 }
 
-fn main() {
-    let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First question".to_string(),
-        "The content of the first question".to_string(),
-        Some(vec!["faq".to_string()]),
-    );
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
 
-    println!("Question: {:?}", question);
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Store {
+    questions: HashMap<QuestionId, Question>,
+}
+
+impl Store {
+    fn new() -> Self {
+        Store {
+            questions: HashMap::new(),
+        }
+    }
+
+    fn init(self) -> HashMap<QuestionId, Question> {
+        let file = include_str!("../questions.json");
+        serde_json::from_str(file).expect("Cannot read questions.json");
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let store = Store::new().init();
+    let store_filter = warp::any().map(move || store.clone());
+
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+
+    let routes = get_items.with(cors);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(_invalid_id) = r.find::<Request<InvalidId>>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
+    }
 }
