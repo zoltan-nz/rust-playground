fe8e262bbb3a6e071d0d58e819fc68ee211bb3bf
Zoltan <zdebre@gmail.com>
2025-03-16 23:36:25 -0400

    feat(qna-axum): add axum server and run on port 3000

---
+++ qna-axum/README.md
@@ -0,0 +1,19 @@
+# Q & A Application using Axum Crate in Rust
+
+Setup default axum server.
+
+Add CORS support.
+
+Required models:
+- Question
+- QuestionId
+
+Create a Store.
+- We need to use async Arc and RwLock to store, read, create and update questions.
+```rust
+use std::sync::Arc;
+use tokio::sync::RwLock;
+```
+
+Read Questions from JSON file as mock data. 
+
--- qna-axum/src/main.rs
+++ qna-axum/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Server is running..."
 }
